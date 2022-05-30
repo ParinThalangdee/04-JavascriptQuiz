78ebdfc6dac6eedb9c1b6a1811da049c12f2cb16
Parin Thalangdee <lotteman10@gmail.com>
2022-05-29 20:36:47 -0700

    Seventh Commit - Troubleshooting 404 issue

--- Assets/highscore.js
+++ Assets/highscore.js
@@ -4,7 +4,7 @@ let highScoresTag = '';
 
 for (let i = 0; i < localStorage.length; i++) {
   highScoreData = JSON.parse(localStorage.getItem(i + 1));
-  highScoresTag = highScoresTag.concat('</br><div id="high-scores">' + highScoreData.initials + " " + "-" + " " + highScoreData.score + '</div>');
+  highScoresTag = highScoresTag.concat('id="high-scores">' + highScoreData.initials + " - " + highScoreData.score + '</div>');
 };
 
 highScoresText.innerHTML = highScoresTag;
